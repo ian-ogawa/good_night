bf3cd500f9f29ebac82f37916a0051b1d6195157
ian-ogawa <ogawa.ian.dev@gmail.com>
2023-06-05 10:15:19 +0700

    add create method for clock in, add current user method to app controller

--- app/controllers/v1/clock_ins_controller.rb
+++ app/controllers/v1/clock_ins_controller.rb
@@ -1,2 +1,22 @@
 class V1::ClockInsController < ApplicationController
+	before_action :clock_in_params
+
+	def create
+		clock_in = ClockIn.new(clock_in_params)
+    if clock_in.save
+      status = :created
+      clock_ins = current_user(clock_in_params[:user_id]).clock_ins
+    else
+      status = :unprocessable_entity
+      message = clock_in.errors.full_messages
+      clock_ins = []
+    end
+
+    json_response({ clock_ins: clock_ins }, status, message)
+	end
+
+	private
+		def clock_in_params
+			params.require(:clock_in).permit(:user_id, :clock_type)
+		end
 end
