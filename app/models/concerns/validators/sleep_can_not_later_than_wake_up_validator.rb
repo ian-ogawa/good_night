ffa7aa5de88b66291778ef939e0ad922006e1e7b
ian-ogawa <ogawa.ian.dev@gmail.com>
2023-06-05 10:22:21 +0700

    fixed bugs validation of sleep can not later than weake up

--- app/models/concerns/validators/sleep_can_not_later_than_wake_up_validator.rb
+++ app/models/concerns/validators/sleep_can_not_later_than_wake_up_validator.rb
@@ -2,6 +2,6 @@ class Validators::SleepCanNotLaterThanWakeUpValidator < ActiveModel::Validator
 	def validate(record)
 		last_sleep = record.user.clock_ins.sleeps.first
 		last_wakeup = record.user.clock_ins.wake_ups.first
-		record.errors.add(:clock_type, "sleep can't later than wake up") if last_sleep.clock_time < last_wakeup.clock_time
+		record.errors.add(:clock_type, "sleep can't later than wake up") if last_sleep.clock_time > last_wakeup.clock_time
 	end
 end
