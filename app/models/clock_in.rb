fdd56ccdfdb36aaa7b35156f8d28cda683fee73a
ian-ogawa <ogawa.ian.dev@gmail.com>
2023-06-05 10:10:33 +0700

    add default scope order, enum for clock type, scope for sleep and wake up clockeid in and callback to set clock time

--- app/models/clock_in.rb
+++ app/models/clock_in.rb
@@ -1,3 +1,17 @@
 class ClockIn < ApplicationRecord
+	default_scope { order(created_at: :desc) }
+
   belongs_to :user
+
+  enum clock_type: { sleep: 'sleep', wake_up: 'wake_up' }
+
+  scope :sleeps, ->{ where(clock_type: :sleep) }
+  scope :wake_ups, ->{ where(clock_type: :wake_up) }
+
+  before_save :set_clock_time
+
+  private
+  	def set_clock_time
+  		self.clock_time = Time.now
+  	end
 end
